289725f64e2b13a0fd7274d30b88af970576e0ff
bilalanwar-wdevep <bilal@qltyss.com>
2025-06-25 15:49:37 +0300

    update the loading in inventory section

--- services/barns-dashboard/src/pages/inventory/index.jsx
+++ services/barns-dashboard/src/pages/inventory/index.jsx
@@ -613,7 +613,7 @@ const InventoryPage = () => {
 
       {/* Loading Overlay */}
       {isLoading && (
-        <div className="fixed inset-0 bg-black bg-opacity-20 flex items-center justify-center z-50">
+        <div className="fixed inset-0  flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-lg shadow-xl">
             <div className="flex items-center space-x-3">
               <svg
