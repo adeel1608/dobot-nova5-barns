3413899910dddd4962761ca52a034bac3d9d631f
adeelqss <adeel@qltyss.com>
2025-09-03 14:03:44 +0300

    updates

--- setup_robot_env.sh
+++ setup_robot_env.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Convenience script to source robot environment from repository root
-source "$(dirname "${BASH_SOURCE[0]}")/services/robot_container/ros_ws/setup_robot_env.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/services/robot_container/ros_ws/setup_robot_env.sh" 
